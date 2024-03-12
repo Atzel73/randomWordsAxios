bed063bae2bf16655ae66b716fbbf96e16451036
Atzel73 <79029695+Atzel73@users.noreply.github.com>
2024-03-12 11:39:11 -0600

    img loaded

--- assets/imgFunction.js
+++ assets/imgFunction.js
@@ -6,8 +6,8 @@ import { Image } from "react-native";
 export const getImg = () => {
     return(
         <Image
-            source={{uri: 'asset:/app_icon.png'}}
-            style={{width: 40, height: 40}}
+            source={{uri: 'https://i.pinimg.com/564x/4c/9e/eb/4c9eeba3088dff9d33c2082252206929.jpg'}}
+            style={{width: 40, height: 40, alignSelf: 'center'}}
         />
     );
 }
